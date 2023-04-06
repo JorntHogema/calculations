abe9e9628663b56dc3d44cb4a288ac2c3cfc9011
JorntHogema <129982694+JorntHogema@users.noreply.github.com>
2023-04-06 14:07:38 +0200

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -15,13 +15,16 @@
 """TODO: Add a description here."""
 
 
-import csv
+#import csv
 import json
-import os
+#import os
 
 import datasets
 from datasets import load_dataset
+from datasets.tasks import QuestionAnsweringExtractive
 load_dataset("https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/calculations.py")
+
+logger = datasets.logging.get_logger(__name__)
 
 # TODO: Add BibTeX citation
 # Find for instance the citation on arxiv or on the dataset repo/website
@@ -41,7 +44,7 @@ This new dataset is designed to solve this great NLP task and is crafted with a 
 """
 
 # TODO: Add a link to an official homepage for the dataset here
-_HOMEPAGE = ""
+_HOMEPAGE = "https://github.com/JorntHogema/calculations"
 
 # TODO: Add the licence for the dataset here if you can find it
 _LICENSE = ""
@@ -49,133 +52,87 @@ _LICENSE = ""
 # TODO: Add link to the official dataset URLs here
 # The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
 # This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
+_URL = "https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/"
 _URLS = {
-    "first_domain": "https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/calculations.json",
-    "second_domain": "https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/calculations-dev.json",
+    "train": _URL + "calculations.json",
+    "dev": _URL + "calculations-dev.json"
 }
 
 
-# TODO: Name of the dataset usually matches the script name with CamelCase instead of snake_case
-class NewDataset(datasets.GeneratorBasedBuilder):
-    """TODO: Short description of my dataset."""
+class CalculationsConfig(datasets.BuilderConfig):
+    """BuilderConfig Calculations"""
 
-    VERSION = datasets.Version("1.1.0")
+    def __init__(self, **kwargs):
+        """BuilderCOnfig for Calulations
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(CalculationsConfig, self).__init__(**kwargs)
 
-    # This is an example of a dataset with multiple configurations.
-    # If you don't want/need to define several sub-sets in your dataset,
-    # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
+class Calculations(datasets.GeneratorBasedBuilder):
+    """Calculations: the calculation answers dataset"""
 
-    # If you need to make complex sub-parts in the datasets with configurable options
-    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
-    # BUILDER_CONFIG_CLASS = MyBuilderConfig
-
-    # You will be able to load one or the other configurations in the following list with
-    # data = datasets.load_dataset('my_dataset', 'first_domain')
-    # data = datasets.load_dataset('my_dataset', 'second_domain')
     BUILDER_CONFIGS = [
-        datasets.BuilderConfig(name="first_domain", version=VERSION, description="This part of my dataset covers a first domain"),
-        datasets.BuilderConfig(name="second_domain", version=VERSION, description="This part of my dataset covers a second domain"),
+        CalculationsConfig(
+        name="plain_text",
+        version=datasets.version("1.0.0",""),
+        description="plain_text",
+        ),
     ]
 
-    DEFAULT_CONFIG_NAME = "first_domain"  # It's not mandatory to have a default configuration. Just use one if it make sense.
-
     def _info(self):
-        # TODO: This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
-        if self.config.name == "first_domain":  # This is the name of the configuration selected in BUILDER_CONFIGS above
-            features = datasets.Features(
-                {
-                    "id": datasets.Value("string"),
-                    "title": datasets.Value("string"),
-                    "context": datasets.Value("string"),
-                    "question": datasets.Value("string"),
-                    "answer": datasets.Value("string")
-                    # These are the features of your dataset like images, labels ...
-                }
-            )
-        else:  # This is an example to show how to have different features for "first_domain" and "second_domain"
-            features = datasets.Features(
-                {
-                    "id": datasets.Value("string"),
-                    "title": datasets.Value("string"),
-                    "context": datasets.Value("string"),
-                    "question": datasets.Value("string"),
-                    "answer": datasets.Value("string")
-                    # These are the features of your dataset like images, labels ...
-                }
-            )
         return datasets.DatasetInfo(
-            # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
-            # This defines the different columns of the dataset and their types
-            features=features,  # Here we define them above because they are different between the two configurations
-            # If there's a common (input, target) tuple from the features, uncomment supervised_keys line below and
-            # specify them. They'll be used if as_supervised=True in builder.as_dataset.
-            # supervised_keys=("sentence", "label"),
-            # Homepage of the dataset for documentation
-            homepage=_HOMEPAGE,
-            # License for the dataset if available
-            license=_LICENSE,
-            # Citation for the dataset
-            citation=_CITATION,
+            features = datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "title": datasets.Value("string"),
+                    "context": datasets.Value("string"),
+                    "question": datasets.Value("string"),
+                    "answer": datasets.Value("string"),
+                    # These are the features of your dataset like images, labels ...
+                }
+            ),
+        supervised_keys=None,
+        homepage=_HOMEPAGE,
+        # License for the dataset if available
+        license=_LICENSE,
+        # Citation for the dataset
+        citation=_CITATION,
+        task_templates=[
+                QuestionAnsweringExtractive(
+                    question_column="question", context_column="context", answers_column="answers"
+                )
+            ],        
         )
+    
+        def _split_generators(self, dl_manager):
+        downloaded_files = dl_manager.download_and_extract(_URLS)
 
-    def _split_generators(self, dl_manager):
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
-        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
-
-        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
-        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
-        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
-        urls = _URLS[self.config.name]
-        data_dir = dl_manager.download_and_extract(urls)
         return [
-            datasets.SplitGenerator(
-                name=datasets.Split.TRAIN,
-                # These kwargs will be passed to _generate_examples
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "train.jsonl"),
-                    "split": "train",
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.VALIDATION,
-                # These kwargs will be passed to _generate_examples
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "dev.jsonl"),
-                    "split": "dev",
-                },
-            ),
-            datasets.SplitGenerator(
-                name=datasets.Split.TEST,
-                # These kwargs will be passed to _generate_examples
-                gen_kwargs={
-                    "filepath": os.path.join(data_dir, "test.jsonl"),
-                    "split": "test"
-                },
-            ),
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
+            datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
         ]
-
+        
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
-    def _generate_examples(self, filepath, split):
+    def _generate_examples(self, filepath):
         # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
-        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
+        # The `key` is for legacy reasons (tfds) and is not importnt in itself, but must be unique for each example.
+        logger.info("generating examples from = %s", filepath)
+        key = 0
         with open(filepath, encoding="utf-8") as f:
-            for key, row in enumerate(f):
-                data = json.loads(row)
-                if self.config.name == "first_domain":
-                    # Yields examples as (key, example) tuples
-                    yield key, {
-                        "title": title,
-                        "context": context,
-                        "question": qa["question"],
-                        "id": qa["id"],
-                        "answers": "" if split == "test" else data["answers"],
-                    }
-                else:
-                    yield key, {
-                        "title": title,
-                        "context": context,
-                        "question": qa["question"],
-                        "id": qa["id"],
-                        "answers": "" if split == "test" else data["answers"],
-                    }
+            data = json.loads(f)
+            for article in data["data"]:
+                title = article.get("title", "")
+                for paragraph in article["paragraphs"]:
+                    context = paragraph["context"]
+                    for qa in paragraph["qas"]:
+                        answers = [answers["text"] fro answers in qa["answers"]]
+                        yield key, {
+                            "title": title,
+                            "context": context,
+                            "question": qa["question"],
+                            "id": qa["id"],
+                            "answers": "" if split == "test" else data["answers"],
+                        }
+                        key += 1
