c02d4a04634cf19ce75a3db96da5545afeb68ba4
JorntHogema <129982694+JorntHogema@users.noreply.github.com>
2023-04-06 14:37:05 +0200

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -21,7 +21,7 @@ import json
 import datasets
 from datasets import load_dataset
 from datasets.tasks import QuestionAnsweringExtractive
-load_dataset("https://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:x-oauth-basic@github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/")
+load_dataset("https://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:x-oauth-basic@github.com/JorntHogema/calculations/")
 
 logger = datasets.logging.get_logger(__name__)
 
