ab186f5b25aae51221b6acb532d14c04baff755b
JorntHogema <129982694+JorntHogema@users.noreply.github.com>
2023-04-06 16:14:24 +0200

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,4 +1,5 @@
-# Copyright 2020 The HuggingFace Datasets Authors and the current dataset script contributor.
+# coding=utf-8
+# Copyright 2020 The TensorFlow Datasets Authors and the HuggingFace Datasets Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,99 +12,91 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# TODO: Address all TODOs and remove all explanatory comments
-"""TODO: Add a description here."""
 
-#import csv
+# Lint as: python3
+"""SQUAD: The Stanford Question Answering Dataset."""
+
+
 import json
-#import os
 
 import datasets
-from datasets import load_dataset
 from datasets.tasks import QuestionAnsweringExtractive
-#load_dataset("https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/")
+
 
 logger = datasets.logging.get_logger(__name__)
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
+
 _CITATION = """\
-@InProceedings{huggingface:dataset,
-title = {A great new dataset},
-author={huggingface, Inc.
-},
-year={2020}
+@article{2016arXiv160605250R,
+       author = {{Rajpurkar}, Pranav and {Zhang}, Jian and {Lopyrev},
+                 Konstantin and {Liang}, Percy},
+        title = "{SQuAD: 100,000+ Questions for Machine Comprehension of Text}",
+      journal = {arXiv e-prints},
+         year = 2016,
+          eid = {arXiv:1606.05250},
+        pages = {arXiv:1606.05250},
+archivePrefix = {arXiv},
+       eprint = {1606.05250},
 }
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """\
-This new dataset is designed to solve this great NLP task and is crafted with a lot of care.
+Stanford Question Answering Dataset (SQuAD) is a reading comprehension \
+dataset, consisting of questions posed by crowdworkers on a set of Wikipedia \
+articles, where the answer to every question is a segment of text, or span, \
+from the corresponding reading passage, or the question might be unanswerable.
 """
-
-# TODO: Add a link to an official homepage for the dataset here
-_HOMEPAGE = "https://github.com/JorntHogema/calculations"
-
-# TODO: Add the licence for the dataset here if you can find it
-_LICENSE = ""
-
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
 _URL = "https://github.com/JorntHogema/calculations/blob/e010d348f0c4ac89e154198dbc7bc4d1026e8204/"
 _URLS = {
     "train": _URL + "calculations.json",
-    "dev": _URL + "calculations-dev.json"
+    "dev": _URL + "calculations-dev.json",
 }
 
-
-class CalculationsConfig(datasets.BuilderConfig):
-    """BuilderConfig Calculations"""
-
+class SquadConfig(datasets.BuilderConfig):
+    """BuilderConfig for SQUAD."""
     def __init__(self, **kwargs):
-        """BuilderCOnfig for Calulations
+        """BuilderConfig for SQUAD.
         Args:
           **kwargs: keyword arguments forwarded to super.
         """
-        super(CalculationsConfig, self).__init__(**kwargs)
+        super(SquadConfig, self).__init__(**kwargs)
 
-class Calculations(datasets.GeneratorBasedBuilder):
-    """Calculations: the calculation answers dataset"""
+
+class Squad(datasets.GeneratorBasedBuilder):
+    """SQUAD: The Stanford Question Answering Dataset. Version 1.1."""
 
     BUILDER_CONFIGS = [
-        CalculationsConfig(
-        name="plain_text",
-        #version=datasets.version("1.0.0",""),
-        description="plain_text",
+        SquadConfig(
+            name="plain_text",
+            version=datasets.Version("1.0.0", ""),
+            description="Plain text",
         ),
     ]
 
     def _info(self):
         return datasets.DatasetInfo(
             description=_DESCRIPTION,
-            features = datasets.Features(
+            features=datasets.Features(
                 {
                     "id": datasets.Value("string"),
                     "title": datasets.Value("string"),
                     "context": datasets.Value("string"),
                     "question": datasets.Value("string"),
-                    "answer": datasets.Value("string"),
-                    # These are the features of your dataset like images, labels ...
+                    "answers": datasets.Value("string"),
                 }
             ),
-        supervised_keys=None,
-        homepage=_HOMEPAGE,
-        # License for the dataset if available
-        license=_LICENSE,
-        # Citation for the dataset
-        citation=_CITATION,
-        task_templates=[
+            # No default supervised_keys (as we have to pass both question
+            # and context as input).
+            supervised_keys=None,
+            homepage="https://rajpurkar.github.io/SQuAD-explorer/",
+            citation=_CITATION,
+            task_templates=[
                 QuestionAnsweringExtractive(
                     question_column="question", context_column="context", answers_column="answers"
                 )
-            ],        
-        ),
+            ],
+        )
+
     def _split_generators(self, dl_manager):
         downloaded_files = dl_manager.download_and_extract(_URLS)
 
@@ -111,7 +104,7 @@ class Calculations(datasets.GeneratorBasedBuilder):
             datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": downloaded_files["train"]}),
             datasets.SplitGenerator(name=datasets.Split.VALIDATION, gen_kwargs={"filepath": downloaded_files["dev"]}),
         ]
-        
+
     def _generate_examples(self, filepath):
         """This function returns the examples in the raw (text) form."""
         logger.info("generating examples from = %s", filepath)
@@ -133,5 +126,5 @@ class Calculations(datasets.GeneratorBasedBuilder):
                             "question": qa["question"],
                             "id": qa["id"],
                             "answers": answers,
-                            }
+                        }
                         key += 1
